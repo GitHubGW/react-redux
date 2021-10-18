db6a785cd482579f0aeeab4c0f3bbc63edf07682
GW <kowonp@gmail.com>
2021-10-18 22:03:16 +0900

    Added Delete Button

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,32 @@
-const Detail = () => {
-  return "Detail";
+import { connect } from "react-redux";
+import { DELETE_TODO } from "../store";
+
+const Detail = ({ state, dispatch, history }) => {
+  const handleDeleteToDo = (state, dispatch, history) => {
+    dispatch({ type: DELETE_TODO, id: state.id });
+    return history.push("/");
+  };
+
+  return (
+    <>
+      <h1>🚗 {state?.text}</h1>
+      <h3>⏰ {state?.id}</h3>
+      <button onClick={() => handleDeleteToDo(state, dispatch, history)}>Delete</button>
+    </>
+  );
 };
 
-export default Detail;
+const mapStateToProps = (state, ownProps) => {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  return { state: state?.find((state) => state.id === +id) };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return { dispatch };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
